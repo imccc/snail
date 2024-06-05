585b57c921a4a59a43b547aa9dbe20ae2aa81d00
sam <sam@imccc.local>
2024-06-05 15:12:13 +0800

    fixed bugs

--- src/interfaces/ViewInterface.php
+++ src/interfaces/ViewInterface.php
@@ -4,13 +4,6 @@ namespace Imccc\Snail\Interfaces;
 
 interface ViewInterface
 {
-    /**
-     * 分配数据给视图
-     *
-     * @param string|array $key 参数键名或参数数组
-     * @param mixed $value 参数值（仅在第一个参数为键名时有效）
-     */
-    public function assign($key, $value = null): void;
 
     /**
      * 显示视图
